48c6679d53aec2c5548bf6e630cbe285326c5f87
Kevin Williams <kevwil@gmail.com>
2012-01-23 21:00:32 -0700

    minor tweaks - add 'index', move primary code to lib folder, make engines more flexible (until someone complains)

--- test/easyhttp.test.js
+++ test/easyhttp.test.js
@@ -1,6 +1,6 @@
 var vows = require('vows'),
     assert = require('assert'),
-    client = require('../easyhttp'),
+    client = require('../lib/easyhttp'),
     http = require('http');
 
 // dummy server
